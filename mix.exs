a59550cbd27f72acfcc70b89b4a03b1179e6d0cd
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-02-13 11:12:10 +0100

    Added CircleCI integration.

--- mix.exs
+++ mix.exs
@@ -1,15 +1,30 @@
 defmodule Case2.MixProject do
   use Mix.Project
 
+  @app :case2
+  @github "am-kantox/#{@app}"
+  @version "0.1.0"
+
   def project do
     [
-      app: :case2,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      app: @app,
+      version: @version,
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      xref: [exclude: []]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -23,7 +38,45 @@ defmodule Case2.MixProject do
     [
       {:recase, "~> 0.4", only: :dev, runtime: false},
       {:benchfella, "~> 0.3", only: :dev, runtime: false}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    Drop-in replacement for [`Recase`](https://github.com/sobolevn/recase)
+    supporting Unicode [Default Identifiers](https://hexdocs.pm/elixir/unicode-syntax.html#r1-default-identifiers).
+    """
+  end
+
+  defp package do
+    [
+      name: @app,
+      files: ["lib", "config", "mix.exs", "README*"],
+      maintainers: ["Aleksei Matiushkin"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/#{@github}",
+        "Docs" => "http://hexdocs.pm/@{app}"
+      }
+    ]
+  end
+
+  defp docs() do
+    [
+      # main: "intro",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/#{@app}",
+      # logo: "stuff/images/logo.png",
+      source_url: "https://github.com/#{@github}",
+      # extras: ["stuff/pages/intro.md"],
+      groups_for_modules: [
+        # Pyc
+
+        # Extras: [
+        #   Iteraptor.Iteraptable,
+        #   Iteraptor.Extras
+        # ],
+      ]
     ]
   end
 end
